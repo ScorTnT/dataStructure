92f5c0a7e49df6fae563e2da1794c9effd024834
ScorTnT <flalxlemdkdlel@naver.com>
2023-03-15 16:14:31 +0900

    first work

---
+++ 1st/CProgram2.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main(){
+//     char NameS[30]={0};                             //추후 0이 아닌 다른 숫자가 입력 될 수 있기에 문자열 초기화 
+//     char NumS[10]={0};                              
+//     printf("학번을 입력하시오. : ");                
+//     scanf("%s",NumS);
+//     printf("이름을 영문으로 입력하시오. : ");
+//     scanf("%s",NameS);
+//     printf("학번 : %s, 이름 : %s\n", NumS,NameS);
+//     int put=3;
+//     printf("종료하려면 숫자 0을 입력하시오.");
+//     scanf("%d",&put);
+//     if(put==0){
+//     return 0;
+//     }else main();
+// }
